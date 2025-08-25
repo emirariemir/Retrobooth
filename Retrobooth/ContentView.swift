8f174f597810ffbd162966f04474fe7efbb39ae2
Emir Arı <emirariomatic@gmail.com>
2025-08-25 23:05:11 +0300

    complete basic UI

--- Retrobooth/ContentView.swift
+++ Retrobooth/ContentView.swift
@@ -5,18 +5,70 @@
 //  Created by Emir Arı on 25.08.2025.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
+    @State private var processedImage: Image?
+    @State private var filterIntensity = 0.5
+    @State private var selectedPhoto: PhotosPickerItem?
+    
+    @State private var filter = CIFilter.sepiaTone()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Spacer()
+                
+                PhotosPicker(selection: $selectedPhoto) {
+                    if let processedImage {
+                        processedImage
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        ContentUnavailableView("No Picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+                    }
+                }
+                .buttonStyle(.plain)
+                .onChange(of: selectedPhoto, loadImage)
+                
+                Spacer()
+                
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                }
+                .padding(.vertical)
+                
+                HStack {
+                    Button("Change filter", action: changeFilter)
+                    
+                    Spacer()
+                    
+                    // button for sharing here
+                }
+                
+            }
+            .padding([.horizontal, .bottom])
+            .navigationTitle("Retrobooth")
         }
-        .padding()
     }
+    
+    func changeFilter() {
+        
+    }
+    
+    func loadImage() {
+        Task {
+            guard let imageData = try await selectedPhoto?.loadTransferable(type: Data.self) else { return }
+            guard let inputImage = UIImage(data: imageData) else { return }
+
+            // more code to come
+        }
+    }
+
 }
 
 #Preview {
