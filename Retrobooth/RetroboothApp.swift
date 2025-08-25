041ba0a388af11587375f4273cd009cb05c568a7
Emir Arı <emirariomatic@gmail.com>
2025-08-25 16:41:33 +0300

    Initial Commit

---
+++ Retrobooth/RetroboothApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetroboothApp.swift
+//  Retrobooth
+//
+//  Created by Emir Arı on 25.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct RetroboothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RetroboothTests/RetroboothTests.swift
@@ -0,0 +1,16 @@
+//
+//  RetroboothTests.swift
+//  RetroboothTests
+//
+//  Created by Emir Arı on 25.08.2025.
+//
+
+import Testing
+
+struct RetroboothTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
